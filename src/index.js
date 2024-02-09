06928d98f4d12ef9d7562e81577ba2457752567e
oaksvirals <oaksvirals@gmail.com>
2024-02-09 12:37:18 -0600

    Finished Weather App
    Completed all TOP requirements for the app.

--- src/index.js
+++ src/index.js
@@ -22,7 +22,9 @@ let gustKPH,
   visKM,
   visMiles,
   windKPH,
-  windMPH;
+  windMPH,
+  weatherIcon,
+  conditionText;
 
 async function initWeatherData() {
   try {
@@ -41,19 +43,39 @@ async function initWeatherData() {
     pressureIN = activeLocation.current.pressure_in;
     pressureMB = activeLocation.current.pressure_mb;
     uv = activeLocation.current.uv;
-    visKM = activeLocation.current.visKM;
-    visMiles = activeLocation.current.visMiles;
+    visKM = activeLocation.current.vis_km;
+    visMiles = activeLocation.current.vis_miles;
     windKPH = activeLocation.current.wind_kph;
     windMPH = activeLocation.current.wind_mph;
     country = activeLocation.location.country;
     name = activeLocation.location.name;
     region = activeLocation.location.region;
+    weatherIcon = activeLocation.current.condition.icon;
+    conditionText = activeLocation.current.condition.text;
   } catch {
     console.log("error with initWeatherData");
   }
 }
 
-initWeatherData();
+(async function onLoad() {
+  await initWeatherData();
+  tempActual.textContent = tempF + "°F";
+  tempFeelsLike.textContent = feelsF + "°F";
+  percipitation.textContent = precipIN + " in";
+  humid.textContent = humidity;
+  pressure.textContent = pressureIN + " in";
+  visibility.textContent = visMiles + " mi";
+  wind.textContent = windMPH + " mph";
+  uvIndex.textContent = uv;
+
+  headingCountry.textContent = country;
+  headingLocation.textContent = name + ", " + region;
+
+  weatherImage.style.backgroundImage = `url(${weatherIcon})`;
+  conditionTxt.textContent = conditionText;
+})();
+
+// initWeatherData();
 
 async function initWeatherLocation(location) {
   try {
@@ -72,30 +94,85 @@ async function initWeatherLocation(location) {
     pressureIN = activeLocation.current.pressure_in;
     pressureMB = activeLocation.current.pressure_mb;
     uv = activeLocation.current.uv;
-    visKM = activeLocation.current.visKM;
-    visMiles = activeLocation.current.visMiles;
+    visKM = activeLocation.current.vis_km;
+    visMiles = activeLocation.current.vis_miles;
     windKPH = activeLocation.current.wind_kph;
     windMPH = activeLocation.current.wind_mph;
     country = activeLocation.location.country;
     name = activeLocation.location.name;
     region = activeLocation.location.region;
-
-    country = activeLocation.location.country;
-    name = activeLocation.location.name;
-    region = activeLocation.location.region;
+    weatherIcon = activeLocation.current.condition.icon;
+    conditionText = activeLocation.current.condition.text;
   } catch {
     console.log("error with initWeatherLocation");
   }
 }
 
-const search = document.querySelector("input");
-const button = document.querySelector("button");
+const tempActual = document.querySelector("#temp-actual");
+const tempFeelsLike = document.querySelector("#temp-feelslike");
+const percipitation = document.querySelector("#percipitation");
+const humid = document.querySelector("#humidity");
+const pressure = document.querySelector("#pressure");
+const visibility = document.querySelector("#visibility");
+const wind = document.querySelector("#wind");
+const uvIndex = document.querySelector("#uv");
 
-function searchWeather() {
-  initWeatherLocation(search.value);
+// const headingName = document.querySelector("#name");
+// const headingRegion = document.querySelector("#region");
+const headingCountry = document.querySelector("#country");
+const headingLocation = document.querySelector("#location");
+
+const weatherImage = document.querySelector("#weatherImg");
+const conditionTxt = document.querySelector("#conditionText");
+
+const search = document.querySelector("#search");
+const searchBtn = document.querySelector("#searchBtn");
+
+async function searchWeather() {
+  await initWeatherLocation(search.value);
   search.value = "";
+  tempActual.textContent = tempF + "°F";
+  tempFeelsLike.textContent = feelsF + "°F";
+  percipitation.textContent = precipIN + " in";
+  humid.textContent = humidity;
+  pressure.textContent = pressureIN + " in";
+  visibility.textContent = visMiles + " mi";
+  wind.textContent = windMPH + " mph";
+  uvIndex.textContent = uv;
+
+  headingCountry.textContent = country;
+  headingLocation.textContent = name + ", " + region;
+
+  weatherImage.style.backgroundImage = `url(${weatherIcon})`;
+  conditionTxt.textContent = conditionText;
 }
 
-button.addEventListener("click", searchWeather);
+searchBtn.addEventListener("click", searchWeather);
 
-// initWeatherLocation("birmingham");
+const fButton = document.querySelector("#F");
+const cButton = document.querySelector("#C");
+
+function activateF() {
+  fButton.classList.add("activeBtn");
+  cButton.classList.remove("activeBtn");
+  tempActual.textContent = tempF + "°F";
+  tempFeelsLike.textContent = feelsF + "°F";
+  percipitation.textContent = precipIN + " in";
+  pressure.textContent = pressureIN + " in";
+  visibility.textContent = visMiles + " mi";
+  wind.textContent = windMPH + " mph";
+}
+
+function activateC() {
+  cButton.classList.add("activeBtn");
+  fButton.classList.remove("activeBtn");
+  tempActual.textContent = tempC + "°C";
+  tempFeelsLike.textContent = feelsC + "°C";
+  percipitation.textContent = precipMM + " mm";
+  pressure.textContent = pressureMB + " mb";
+  visibility.textContent = visKM + " km";
+  wind.textContent = windKPH + " kph";
+}
+
+fButton.addEventListener("click", activateF);
+cButton.addEventListener("click", activateC);
